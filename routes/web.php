ae1193dc9b485e3f5622b220c7d6b38a12a01bb3
Morshed <skmorshed75@yahoo.com>
2023-12-23 19:01:37 +0600

    Completed Routes with web dot php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\ResumeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +17,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//PAGE ROUTES
+Route::get("/",[HomeController::class,'page']);
+Route::get("/contact",[ContactController::class,'page']);
+Route::get("/prjects",[ProjectController::class,'page']);
+Route::get("/resume",[ResumeController::class,'page']);
+
+//AJAX CALL ROUTES
+//Homepage
+Route::get("/herodata",[HomeController::class,'heroData']);
+Route::get("/aboutData",[HomeController::class,'aboutData']);
+Route::get("/socialData",[HomeController::class,'socialData']);
+
+//Contact Page
+Route::get("/contactRequest",[ContactController::class,'contactRequest']);
+
+//Project Page
+Route::get("/projectData",[ProjectController::class,'projectData']);
+
+//Resume Page
+Route::get("/resumelink",[ResumeController::class,'resumeLink']);
+Route::get("/experiencedata",[ResumeController::class,'experienceData']);
+Route::get("/educationData",[ResumeController::class,'educationData']);
+Route::get("/skillsData",[ResumeController::class,'skillsData']);
+Route::get("/languageData",[ResumeController::class,'languageData']);
