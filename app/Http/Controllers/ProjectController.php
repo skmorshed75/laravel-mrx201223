4a743180aea5b7f0982857e51fb534b507b59d94
Morshed <skmorshed75@yahoo.com>
2023-12-23 18:40:59 +0600

    Create necessary function inside the controllers

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    function page(Request $request){
+
+    }
+
+    function projectData(Request $request){
+        
+    }
+}
