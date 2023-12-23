0aff6e74a35ff6882b3dc0fdac3b4eebbf6c0fa3
Morshed <skmorshed75@yahoo.com>
2023-12-23 19:06:14 +0600

    Create API Routes with api.php

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,31 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\ResumeController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+//AJAX CALL ROUTES
+//Homepage
+Route::get("/herodata",[HomeController::class,'heroData']);
+Route::get("/aboutData",[HomeController::class,'aboutData']);
+Route::get("/socialData",[HomeController::class,'socialData']);
+
+//Contact Page
+Route::get("/contactRequest",[ContactController::class,'contactRequest']);
+
+//Project Page
+Route::get("/projectData",[ProjectController::class,'projectData']);
+
+//Resume Page
+Route::get("/resumelink",[ResumeController::class,'resumeLink']);
+Route::get("/experiencedata",[ResumeController::class,'experienceData']);
+Route::get("/educationData",[ResumeController::class,'educationData']);
+Route::get("/skillsData",[ResumeController::class,'skillsData']);
+Route::get("/languageData",[ResumeController::class,'languageData']);
