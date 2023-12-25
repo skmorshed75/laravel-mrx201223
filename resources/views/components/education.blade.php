ba9a3fb57bd285057b841fe05d3a3b2b4691cb2d
Morshed <skmorshed75@yahoo.com>
2023-12-25 17:47:47 +0600

    Ajax Integration with Languages.blade.php

--- resources/views/components/education.blade.php
+++ resources/views/components/education.blade.php
@@ -1,7 +1,4 @@
 <div class="container px-5 my-5">
-    <div class="text-center mb-5">
-        <h1 class="display-5 fw-bolder mb-0"><span class="text-gradient d-inline">Resume</span></h1>
-    </div>
     <div class="row gx-5 justify-content-center">
         <div class="col-lg-11 col-xl-9 col-xxl-8">
             <!-- Education Section-->
--- resources/views/components/language.blade.php
+++ resources/views/components/language.blade.php
@@ -1,7 +1,4 @@
 <div class="container px-5 my-5">
-    <div class="text-center mb-5">
-        <h1 class="display-5 fw-bolder mb-0"><span class="text-gradient d-inline">Resume</span></h1>
-    </div>
     <div class="row gx-5 justify-content-center">
         <div class="col-lg-11 col-xl-9 col-xxl-8">
             <section>
@@ -14,15 +11,12 @@
                                 <div class="feature bg-primary bg-gradient-primary-to-secondary text-white rounded-3 me-3"><i class="bi bi-code-slash"></i></div>
                                 <h3 class="fw-bolder mb-0"><span class="text-gradient d-inline">Languages</span></h3>
                             </div>
-                            <div class="row row-cols-1 row-cols-md-3 mb-4">
-                                <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">HTML</div></div>
-                                <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">CSS</div></div>
-                                <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">JavaScript</div></div>
-                            </div>
-                            <div class="row row-cols-1 row-cols-md-3">
-                                <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Python</div></div>
-                                <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Ruby</div></div>
-                                <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Node.js</div></div>
+                            <div id = "language-list" class="row row-cols-1 row-cols-md-3 mb-4">
+
+
+                                {{-- CALL AJAX HERE --}}
+
+                                
                             </div>
                         </div>
                     </div>
@@ -33,5 +27,25 @@
 </div>
 
 <script>
-    
+    LanguageList();
+    async function LanguageList(){
+        let URL = "/languageData";
+        try {
+            let response = await axios.get(URL); //API calling with ajax
+            response.data.forEach(element => {
+                document.getElementById('language-list').innerHTML+= (
+                `<div class="col mb-4 p-3 mb-md-0">
+                    <div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">
+                        ${element['name']}
+                        </div>
+                    </div>
+                </div>`
+                )
+            });
+
+        } catch (error) {
+            alert(error);
+        }
+
+    }
 </script>
